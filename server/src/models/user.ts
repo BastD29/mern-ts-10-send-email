4cd317cc0b96f9b826877d80b28a0ad9eedc2758
BastD29 <bastiendaniel9@gmail.com>
2024-07-25 16:52:36 +0200

    user mongoose model

---
+++ server/src/models/user.ts
@@ -0,0 +1,21 @@
+import { Schema, model } from "mongoose";
+import { UserType } from "../types/user";
+import bcrypt from "bcrypt";
+
+const userSchema = new Schema<UserType>({
+  email: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
+  password: { type: String, required: true },
+});
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+userSchema.methods.matchPassword = async function (enteredPassword: string) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+export default model<UserType>("User", userSchema);
--- server/src/types/user.ts
+++ server/src/types/user.ts
@@ -1,8 +1,17 @@
-type UserType = {
-  id: number;
+// type UserType = {
+//   id: number;
+//   email: string;
+//   name: string;
+//   password: string;
+// };
+
+import { Document } from "mongoose";
+
+type UserType = Document & {
   email: string;
   name: string;
   password: string;
+  matchPassword: (enteredPassword: string) => Promise<boolean>;
 };
 
 export type { UserType };
