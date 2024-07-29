dff92db35e359a2cdb4c92a3b8fecc4d4f5d528b
BastD29 <bastiendaniel9@gmail.com>
2024-07-29 15:28:28 +0200

    redirection to dashboard if user

--- client/src/components/layouts/AuthLayout/AuthLayout.tsx
+++ client/src/components/layouts/AuthLayout/AuthLayout.tsx
@@ -1,10 +1,20 @@
-import { FC, Suspense } from "react";
-import { Outlet } from "react-router-dom";
+import { FC, Suspense, useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
 import LoaderLayout from "../LoaderLayout/LoaderLayout";
 import Header from "../../Header/Header2";
 import style from "./AuthLayout.module.scss";
+import { useAuthContext } from "../../../hooks/useAuthContext";
 
 const AuthLayout: FC = () => {
+  const { user } = useAuthContext();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (user) {
+      navigate("/");
+    }
+  }, []);
+
   return (
     <div className={style["auth-layout"]}>
       <Suspense fallback={<LoaderLayout />}>
