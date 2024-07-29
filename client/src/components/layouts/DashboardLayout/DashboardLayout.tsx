3718812d61818909685403a69e797dcf486d2e2c
BastD29 <bastiendaniel9@gmail.com>
2024-07-29 16:25:29 +0200

    redirect to auth layout if no user

--- client/src/components/layouts/DashboardLayout/DashboardLayout.tsx
+++ client/src/components/layouts/DashboardLayout/DashboardLayout.tsx
@@ -1,10 +1,20 @@
-import { FC, Suspense } from "react";
-import { Outlet } from "react-router-dom";
+import { FC, Suspense, useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
 import LoaderLayout from "../LoaderLayout/LoaderLayout";
 import Header from "../../Header/Header2";
+import { useAuthContext } from "../../../hooks/useAuthContext";
 import style from "./DashboardLayout.module.scss";
 
 const DashboardLayout: FC = () => {
+  const { user } = useAuthContext();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!user) {
+      navigate("/auth/signin");
+    }
+  }, []);
+
   return (
     <div className={style["dashboard-layout"]}>
       <Suspense fallback={<LoaderLayout />}>
