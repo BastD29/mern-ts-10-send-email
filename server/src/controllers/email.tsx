f45544b527c52484086301eb4f43b4076e72f4a5
BastD29 <bastiendaniel9@gmail.com>
2024-07-29 19:13:06 +0200

    added try catch

--- server/src/controllers/email.tsx
+++ server/src/controllers/email.tsx
@@ -7,26 +7,31 @@ import nodemailer from "nodemailer";
 // * REPLACE HARD DATA WITH ENVIRONMENTS
 
 const sendEmail = async (req: Request, res: Response) => {
-  const transporter = nodemailer.createTransport({
-    host: "smtp.hostinger.com",
-    port: 465,
-    secure: true,
-    auth: {
-      user: "test@testaroo.net",
-      pass: "i8s4w7Zlwe0F4bdr&",
-    },
-  });
+  try {
+    const transporter = nodemailer.createTransport({
+      host: "smtp.hostinger.com",
+      port: 465,
+      secure: true,
+      auth: {
+        user: "test@testaroo.net",
+        pass: "i8s4w7Zlwe0F4bdr&",
+      },
+    });
 
-  const emailHtml = render(<Email url="https://example.com" />);
+    const emailHtml = render(<Email url="https://example.com" />);
 
-  const options = {
-    from: "test@testaroo.net",
-    to: "miranoc510@maxturns.com",
-    subject: "hello world",
-    html: emailHtml,
-  };
+    const options = {
+      from: "test@testaroo.net",
+      to: "miranoc510@maxturns.com",
+      subject: "hello world",
+      html: emailHtml,
+    };
 
-  await transporter.sendMail(options);
+    await transporter.sendMail(options);
+    res.status(200).send({ message: "Email sent successfully" });
+  } catch (error) {
+    res.status(500).send({ message: "Error sending email" });
+  }
 };
 
 export { sendEmail };
